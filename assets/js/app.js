354d49107c46068ce86ea8ad4b594ad67a4d8e18
liupeng <dp90219@gmail.com>
2014-07-21 17:09:16 +0800

    add singflying banner in sidebar
    change post grid
    
    change post grid
    
    change post grid
    
    add singflying banner in sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,12 @@ $( document ).ready(function() {
 	/* Secondary contact links */
 	var scontacts = $('#contact-list-secondary');
 	var contact_list = $('#contact-list');
+  var singflying = $('.singflying')
 	
 	scontacts.hide();
 	
-	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
+	contact_list.mouseenter(function(){ scontacts.fadeIn('slow'); singflying.delay(200).fadeOut('slow');});
 	
-	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
+	contact_list.mouseleave(function(){ scontacts.fadeOut('slow'); singflying.delay(200).fadeIn('slow');});
 
 });
